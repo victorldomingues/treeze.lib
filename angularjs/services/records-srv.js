c806ba035b412d5d0529e46ef7dfc11d91f70436
Ronan Rocha de Souza <nan.cst@hotmail.com>
2016-08-03 19:23:42 -0300

    adjust endpoints services

--- angularjs/services/records-srv.js
+++ angularjs/services/records-srv.js
@@ -8,7 +8,43 @@
 			else {
 				apiUrl =  treeze_api_url;
 			}
+            
+            
+            
+   angularTzModule.factory('uniqueLoginFactory', ['$http', function ($http) {
+      return function (params) {
+        var login = params.login;
+        var companyId = params.companyId;
+        var accountId = params.id;
+        var url = '';
+        var path = ''
+        if (accountId) {
+          url = apiUrl + "accounts/find-by-login/"+ login;
+        }
+        else {
+          url = apiUrl + "accounts/find-by-login/"+ login;
+        }
+
+        var restResource = $http({ "method": "GET", "url": url });
+        return restResource;
+      }
+    }])
+    
+    //via resource
+    angularTzModule.factory('accountFactory', ['$resource', function ($resource) {
+      return $resource(apiUrl + 'accounts/:id', {}, {
+          get: { method: 'GET' },
+          update: { method: 'PUT' },
+          remove: { method: 'DELETE' }
+        });
+    }])
 	
+    //via querystring
+    angularTzModule.factory('accountsFactory', ['$resource', function ($resource) {
+      return $resource(apiUrl +  'accounts/', {}, {
+          get: { method: 'GET' }
+        });
+    }])
 	
     angularTzModule.factory('accountCustomFormsFactory', ['$resource', function ($resource) {
       return $resource( apiUrl + 'account-custom-forms/:companyId', {}, {
@@ -23,30 +59,29 @@
       })
     }])
 
+
+
+
     angularTzModule.factory('accountPostFactory', ['$resource', function ($resource) {
-      return $resource(apiUrl + 'account/', {}, {
+      return $resource(apiUrl + 'accounts/', {}, {
           save: { method: 'POST', cache: false, isArray: false },
         });
     }])
 
-    angularTzModule.factory('accountsFactory', ['$resource', function ($resource) {
-      return $resource(apiUrl +  'account/:companyId/', {}, {
-          get: { method: 'GET' }
-        });
-    }])
+   
 
-    angularTzModule.factory('accountFactory', ['$resource', function ($resource) {
-      return $resource(apiUrl + 'account/:companyId/:login', {}, {
-          get: { method: 'GET' },
-          update: { method: 'PUT' },
+  
+    
+    angularTzModule.factory('accountDeleteFactory', ['$resource', function ($resource) {
+      return $resource(apiUrl + "accounts/:id/", {id :'@id', reason: '@reason'}, {
           remove: { method: 'DELETE' }
         });
     }])
     
-    angularTzModule.factory('accountDeleteFactory', ['$resource', function ($resource) {
-      return $resource(apiUrl + "account/:companyId/:login/:reason", {}, {
-          remove: { method: 'DELETE' }
-        });
-    }])
+    
+    
 	
 })();
+
+
+
